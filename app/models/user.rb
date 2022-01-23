1454860a8857a9899ca5b721b8948c6a72f44184
Boula Akladyous <akladyous@gmail.com>
2022-01-23 12:22:49 -0500

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
     has_many :listing_infos
     has_many :pets, through: :listing_infos
 
+    # validates :size, inclusion: {in: %w[small medium large], message: "%{value} is not a valid size" }
+    # validates :species, inclusion: {in: %w[dog cat bird ferret pig reptiles horse], message: "%{value} is not a valid species" }
+    # validates :gender, inclusion: {in: %w[male female, unknown], message: "%{value} is not a valid gender" }
 end
