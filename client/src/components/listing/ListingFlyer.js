90c4fefbedb4e963ebc3f9e9d6eb5c19dd9936e3
Boula Akladyous <akladyous@gmail.com>
2022-02-05 20:46:22 -0500

    removed useNavigate

--- client/src/components/listing/ListingFlyer.js
+++ client/src/components/listing/ListingFlyer.js
@@ -39,7 +39,7 @@ export default function ListingFlyer() {
                         })
                 } catch(error){console.error(error.message)}
             })();
-            navigate('/')
+            // navigate('/')
     },[])
     
     return user ? (
