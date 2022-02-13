685a15bbc0c5a89c34b492972d2229afc01c4fdf
Boula Akladyous <akladyous@gmail.com>
2022-02-13 15:10:07 -0500

    deploy #2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
     config.active_storage.service = :amazon
 
-    # config.action_mailer.default_url_option = { host: "http://localhost:3000" }
+    config.action_mailer.default_url_option = { host: "https://petfinder101.herokuapp.com/" }
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_caching = false
     config.action_mailer.perform_deliveries = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
     # Do not dump schema after migrations.
     config.active_record.dump_schema_after_migration = false
 
-    config.action_mailer.default_url_option = { host: "www.lostpet101.com" }
+    config.action_mailer.default_url_option = { host: "https://petfinder101.herokuapp.com/" }
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_caching = false
     config.action_mailer.perform_deliveries = true
