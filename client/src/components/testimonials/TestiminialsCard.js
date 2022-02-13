1b390b403e517aa42671e1331654b9388ed18658
Boula Akladyous <akladyous@gmail.com>
2022-02-13 02:06:21 -0500

    Testimonials - added data verification before rendering

--- client/src/components/testimonials/TestiminialsCard.js
+++ client/src/components/testimonials/TestiminialsCard.js
@@ -1,39 +1,57 @@
-import React from 'react'
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-export default function TestiminialsCard(props) {
-    console.log("props: ", props);
-    // const { data: testimonial } = props || {};
-    // console.log(testimonial)
-    return (
-        <div className="row- d-flex border my-3 signle-testimonial">
+import React, { useState } from 'react'
+export default function TestiminialsCard({ testimonial }) {
+    
+    // console.log("props: ", testimonial);
+    // const { pet: { image_url: petImage, name: petName }, listing_comment: { comment: petComment = '', likes: petLikes = '' }, } = testimonial || {};
+    // console.log(petImage, petName, petComment, petLikes);
 
-            <div className="col-5 ">
-                <div className="testimonial-img-area">
-                    {/* <img className="img-fluid" src={testimonial.pet.image_url} alt="" /> */}
-                </div>
-            </div>
-            
-            <div className="col-7">
-                <div className="testimonial-detail d-flex flex-column ">
-                    <div className='flex-shrink-0'>
-                        {/* <h2 className='heading'>{testimonial.pet.name.toUpperCase()}</h2> */}
-                    </div>
-                    <div className='h-100 flex-grow-1'>
-                        {/* <p className='text-wrap p-2'>{testimonial.listing_comment.comment}</p> */}
-                    </div>
-                    <div className='flex-shrink-0'>
-                        <button type="button" className="btn position-relative">
-                        
-                        {/* <i className='fa-solid fa-heart fa-1x'> */}
-                            <span className="badge position-absolute top-0 start-100 translate-middle rounded-pill ">
-                                {/* {testimonial.listing_comment.likes} */}
-                                <span className="visually-hidden">unread messages</span>
-                            </span>
-                        {/* </i> */}
-                        </button>
+    if (testimonial.pet === null || testimonial.listing_comment === null) {
+        return <></>;
+    } else {
+        return (
+            <div className="row- d-flex border my-3 signle-testimonial">
+                <div className="col-5 ">
+                    <div className="testimonial-img-area">
+                        <img
+                            className="img-fluid"
+                            src={testimonial.pet.image_url}
+                            alt=""
+                        />
                     </div>
                 </div>
+
+                <div className="col-7">
+                    <div className="testimonial-detail d-flex flex-column ">
+                        <div className="flex-shrink-0">
+                            <h2 className="heading">
+                                {testimonial.pet.name.toUpperCase()}
+                            </h2>
+                        </div>
+                        <div className="h-100 flex-grow-1">
+                            <p className="text-wrap p-2">
+                                {testimonial.listing_comment.comment}
+                            </p>
+                        </div>
+                        <div className="flex-shrink-0">
+                            <button
+                                type="button"
+                                className="btn position-relative"
+                            >
+                                <i className='fa-solid fa-heart fa-1x'>
+                                    <span className="badge position-absolute top-0 start-100 translate-middle rounded-pill">
+                                        {testimonial.listing_comment.likes}
+                                        <span className="visually-hidden">
+                                            unread messages
+                                        </span>
+                                    </span>
+                                </i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    
 }
--- client/src/components/testimonials/TestimonialsMapper.js
+++ client/src/components/testimonials/TestimonialsMapper.js
@@ -3,8 +3,8 @@ export default function TestimonialsMapper(props) {
     const { itemComponent: ItemComponent, data } = props || {};
 
     return data ? 
-        data.map((item, index) => (
-            <ItemComponent key={item.id} testimonial={item} index={index} />
+        data.map((item) => (
+            <ItemComponent key={item.id} testimonial={item} />
         ))
         : null;
 }
